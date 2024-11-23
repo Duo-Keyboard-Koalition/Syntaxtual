759b7e04cd454b4173f2fc66a3b0e5fa28e6629f
Darcy Liu <darcy.ldx@gmail.com>
2024-11-23 12:51:21 -0500

    added the helper component functions

--- plugin/src/extension.ts
+++ plugin/src/extension.ts
@@ -1,39 +1,25 @@
 import * as vscode from 'vscode';
+import { insertDateTime } from './components/insertDateTime';
+import { printFirstTenChars } from './components/printFirstTenChars';
+import { simulateApiCall } from './components/simulateApiCall';
+import { onFileChange, clearFileChangeTimeout } from './components/fileChange';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "sample-date-inserter" is now active!');
+    console.log('Congratulations, your extension "sample-date-inserter" is now active!');
 
-	let insertDateTimeDisposable = vscode.commands.registerCommand('sample-date-inserter.insertDateTime', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const currentDateTime = new Date().toLocaleString();
-			editor.edit(editBuilder => {
-				editBuilder.insert(editor.selection.active, currentDateTime);
-			}).then(success => {
-				if (success) {
-					vscode.window.showInformationMessage(`Inserted date and time: ${currentDateTime}`);
-				} else {
-					vscode.window.showErrorMessage('Failed to insert date and time');
-				}
-			});
-		} else {
-			vscode.window.showErrorMessage('No active editor found');
-		}
-	});
+    let insertDateTimeDisposable = vscode.commands.registerCommand('sample-date-inserter.insertDateTime', insertDateTime);
+    let printFirstTenCharsDisposable = vscode.commands.registerCommand('sample-date-inserter.printFirstTenChars', printFirstTenChars);
+    let simulateApiCallDisposable = vscode.commands.registerCommand('sample-date-inserter.simulateApiCall', simulateApiCall);
+    let fileChangeDisposable = vscode.workspace.onDidChangeTextDocument(onFileChange);
 
-	let printFirstTenCharsDisposable = vscode.commands.registerCommand('sample-date-inserter.printFirstTenChars', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const document = editor.document;
-			const firstTenChars = document.getText().slice(0, 10);
-			vscode.window.showInformationMessage(`First 10 characters: ${firstTenChars}`);
-		} else {
-			vscode.window.showErrorMessage('No active editor found');
-		}
-	});
-
-	context.subscriptions.push(insertDateTimeDisposable, printFirstTenCharsDisposable);
+    context.subscriptions.push(
+        insertDateTimeDisposable,
+        printFirstTenCharsDisposable,
+        simulateApiCallDisposable,
+        fileChangeDisposable
+    );
 }
 
-export function deactivate() {}
-
+export function deactivate() {
+    clearFileChangeTimeout();
+}
