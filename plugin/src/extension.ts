7fb9558f4fdaf47cfac2e4f87b716669032cca1d
Darcy Liu <darcy.ldx@gmail.com>
2024-11-23 15:30:51 -0500

    renamed and cleaned up the code

--- plugin/src/extension.ts
+++ plugin/src/extension.ts
@@ -1,25 +1,25 @@
 import * as vscode from 'vscode';
-import { insertDateTime } from './components/insertDateTime';
-import { printFirstTenChars } from './components/printChars';
-import { simulateApiCall } from './components/simulateApiCall';
-import { onFileChange, clearFileChangeTimeout } from './components/fileChange';
-import { highlight } from './components/highlight';
+import { insertDateTime } from './components/onWriteEditor';
+import { getEditorContent } from './components/onReadEditor';
+import { getCodeReview } from './components/onReviewEditor';
+import { onUpdateEvent, clearFileChangeTimeout } from './components/onChangeEditor';
+import { highlight } from './components/onHighlightEditor';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "sample-date-inserter" is now active!');
 
     let insertDateTimeDisposable = vscode.commands.registerCommand('sample-date-inserter.insertDateTime', insertDateTime);
-    let printFirstTenCharsDisposable = vscode.commands.registerCommand('sample-date-inserter.printFirstTenChars', printFirstTenChars);
-    let simulateApiCallDisposable = vscode.commands.registerCommand('sample-date-inserter.simulateApiCall', simulateApiCall);
-    let fileChangeDisposable = vscode.workspace.onDidChangeTextDocument(onFileChange);
+    let getEditorContentDisposable = vscode.commands.registerCommand('sample-date-inserter.getEditorContent', getEditorContent);
+    let getCodeReviewDisposable = vscode.commands.registerCommand('sample-date-inserter.getCodeReview', getCodeReview);
+    let fileChangeDisposable = vscode.workspace.onDidChangeTextDocument(onUpdateEvent);
     let highlightDisposable = vscode.commands.registerCommand('sample-date-inserter.highlight', () => {
         highlight(0, 10, 'rgba(255, 255, 0, 0.3)', 'yellow');
     });
 
     context.subscriptions.push(
         insertDateTimeDisposable,
-        printFirstTenCharsDisposable,
-        simulateApiCallDisposable,
+        getEditorContentDisposable,
+        getCodeReviewDisposable,
         fileChangeDisposable,
         highlightDisposable
     );
