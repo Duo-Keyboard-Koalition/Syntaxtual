301bfdf2d3eb31b941fb960c5601de01249e7fab
Darcy Liu <darcy.ldx@gmail.com>
2024-11-23 13:37:42 -0500

    added highlight

--- plugin/src/extension.ts
+++ plugin/src/extension.ts
@@ -3,6 +3,7 @@ import { insertDateTime } from './components/insertDateTime';
 import { printFirstTenChars } from './components/printFirstTenChars';
 import { simulateApiCall } from './components/simulateApiCall';
 import { onFileChange, clearFileChangeTimeout } from './components/fileChange';
+import { highlight } from './components/highlight';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "sample-date-inserter" is now active!');
@@ -11,12 +12,14 @@ export function activate(context: vscode.ExtensionContext) {
     let printFirstTenCharsDisposable = vscode.commands.registerCommand('sample-date-inserter.printFirstTenChars', printFirstTenChars);
     let simulateApiCallDisposable = vscode.commands.registerCommand('sample-date-inserter.simulateApiCall', simulateApiCall);
     let fileChangeDisposable = vscode.workspace.onDidChangeTextDocument(onFileChange);
+    let highlightDisposable = vscode.commands.registerCommand('sample-date-inserter.highlight', highlight);
 
     context.subscriptions.push(
         insertDateTimeDisposable,
         printFirstTenCharsDisposable,
         simulateApiCallDisposable,
-        fileChangeDisposable
+        fileChangeDisposable,
+        highlightDisposable
     );
 }
 
